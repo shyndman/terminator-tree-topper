5fecb22983c63ef685a151416518ef46a0500278
Scott Hyndman <scotty.hyndman@gmail.com>
2023-12-03 23:10:14 -0500

    Update tuning to target silent operation, and complete powerdown

--- src/stepper/tune.rs
+++ src/stepper/tune.rs
@@ -23,6 +23,7 @@ pub async fn tune_driver<M: RawMutex + 'static, P: uart::Instance>(
         // Disable automatic power down, because its pin is shared
         let mut gconf = tmc2209::reg::GCONF::default();
         gconf.set_pdn_disable(true);
+        gconf.set_shaft(true);
         driver.write_register(gconf).await?;
 
         let mut ihold_irun = tmc2209::reg::IHOLD_IRUN::default();
@@ -30,7 +31,7 @@ pub async fn tune_driver<M: RawMutex + 'static, P: uart::Instance>(
             TMC2209_VSENSE_OHMS,
             motor_constants.run_current_milliamps(),
         );
-        let ihold = irun - 8.min(irun);
+        let ihold = 0; // Use passive breaking during standby
         defmt::info!("Setting IRUN={}, IHOLD={}", irun, ihold);
         ihold_irun.set_irun(irun);
         ihold_irun.set_ihold(ihold);
@@ -45,9 +46,8 @@ pub async fn tune_driver<M: RawMutex + 'static, P: uart::Instance>(
         let (hstrt, hend) = motor_constants.hysteresis(None, None, None, None);
         chopconf.set_hstrt(hstrt);
         chopconf.set_hstrt(hend);
-        // chopconf.set_mres(0b00); // 256 steps
-        chopconf.set_mres(0b1000); // Fullstep
-        chopconf.set_intpol(false); // Interpolation of microsteps to 256
+        chopconf.set_mres(0b00); // 256 steps
+        chopconf.set_intpol(true); // Interpolation of microsteps to 256
         chopconf.set_vsense(vsense);
         chopconf.set_tbl(1);
         chopconf
@@ -57,8 +57,8 @@ pub async fn tune_driver<M: RawMutex + 'static, P: uart::Instance>(
 
         // Stallguard
         let mut tpwnthrs = tmc2209::reg::TPWMTHRS::default();
-        tpwnthrs.set(0xfffff); // This keeps the stepper in SpreadCycle mode
-                               // tpwnthrs.set(0); // This keeps the stepper in StealthChop mode
+        // tpwnthrs.set(0xfffff); // This keeps the stepper in SpreadCycle mode
+        tpwnthrs.set(0); // This keeps the stepper in StealthChop mode
         driver.write_register(tpwnthrs).await?;
 
         // Coolstep
@@ -67,7 +67,7 @@ pub async fn tune_driver<M: RawMutex + 'static, P: uart::Instance>(
         driver.write_register(tcoolthrs).await?;
 
         let mut sgthrs = tmc2209::reg::SGTHRS::default();
-        sgthrs.0 = 100;
+        sgthrs.0 = 0xff;
         driver.write_register(sgthrs).await?;
 
         // StealthChop configuration
@@ -77,10 +77,9 @@ pub async fn tune_driver<M: RawMutex + 'static, P: uart::Instance>(
         pwmconf.set_pwm_freq(0b10);
         pwmconf.set_pwm_grad(motor_constants.pwm_gradient(None, None) as u8);
         pwmconf.set_pwm_ofs(motor_constants.pwm_output_frequency(None) as u8);
-        // pwmconf.set_pwm_grad(2);
-        // pwmconf.set_pwm_ofs(62);
         pwmconf.set_pwm_reg(15);
         pwmconf.set_pwm_lim(4);
+        pwmconf.set_freewheel(2); // Passive breaking
         driver.write_register(pwmconf).await.unwrap();
 
         let mut coolconf = tmc2209::reg::COOLCONF::default();
@@ -91,10 +90,10 @@ pub async fn tune_driver<M: RawMutex + 'static, P: uart::Instance>(
         coolconf.set_seimin(SEIMIN);
         driver.write_register(coolconf).await?;
 
-        gconf.set_en_spread_cycle(true); // SpreadCycle
-                                         // gconf.set_en_spread_cycle(false); // SpreadCycle
+        // gconf.set_en_spread_cycle(true); // SpreadCycle
+        gconf.set_en_spread_cycle(false); // StealthChop
         gconf.set_i_scale_analog(false);
-        gconf.set_multistep_filt(false);
+        gconf.set_multistep_filt(true);
         gconf.set_mstep_reg_select(true); // Set microsteps via registers
         driver.write_register(gconf).await?;
     };
